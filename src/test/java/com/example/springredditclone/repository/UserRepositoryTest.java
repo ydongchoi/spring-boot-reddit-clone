ade0d0bc629aa10499756f73fd4e849c18cc5600
YD <105weapon@gmail.com>
2023-11-21 17:58:04 +0900

    feat: added shouldFindByUserName Unit Test

--- src/test/java/com/example/springredditclone/repository/UserRepositoryTest.java
+++ src/test/java/com/example/springredditclone/repository/UserRepositoryTest.java
@@ -2,6 +2,7 @@ package com.example.springredditclone.repository;
 
 import com.example.springredditclone.BaseTest;
 import com.example.springredditclone.model.User;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
@@ -11,6 +12,7 @@ import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
 import java.time.Instant;
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -19,13 +21,25 @@ import static org.assertj.core.api.Assertions.assertThat;
 class UserRepositoryTest extends BaseTest {
     @Autowired
     private UserRepository userRepository;
+    private static User expectedUserObject;
+
+    @BeforeAll
+    public static void setup(){
+        expectedUserObject = new User(123L, "test user", "secret password", "user@email.com", Instant.now(), true);
+    }
 
     @Test
     public void shouldSavePost(){
-        User expectedUserObject = new User(123L, "test user", "secret password", "user@email.com", Instant.now(), true);
         User actualUserObject = userRepository.save(expectedUserObject);
         assertThat(actualUserObject).usingRecursiveComparison()
                 .ignoringFields("userId").isEqualTo(expectedUserObject);
     }
 
+    @Test
+    public void shouldFindByUserName(){
+        userRepository.save(expectedUserObject);
+        Optional<User> actualUserObject = userRepository.findByUsername("test user");
+        assertThat(actualUserObject.get()).usingRecursiveComparison()
+                .ignoringFields("userId").isEqualTo(expectedUserObject);
+    }
 }
